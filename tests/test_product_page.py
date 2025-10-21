84ddc51a5db001769d9e9489925affdb45e927a2
VladykaSith <naumov.vladyka@gmail.com>
2025-10-21 14:13:27 +0700

    added parametrization and marked as xfail the parameter that was expected to fail

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,9 +1,17 @@
-from pyexpat.errors import messages
-
-from pages.base_page import BasePage
 from pages.product_page import ProductPage
-def test_user_can_add_item_into_cart(browser):
-    link='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+import pytest
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_user_can_add_item_into_cart(browser,link):
+
     page=ProductPage(browser,link)
     page.open()
     page.add_item_into_cart()
