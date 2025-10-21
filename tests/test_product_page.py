9047dd54f8c4f96fff3b2f99a5c1f08e4774e7cc
VladykaSith <naumov.vladyka@gmail.com>
2025-10-21 13:15:55 +0700

    made the test_product_page.py more independent now it takes the info about the title and price from the product page nad saves as argument

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -3,7 +3,7 @@ from pyexpat.errors import messages
 from pages.base_page import BasePage
 from pages.product_page import ProductPage
 def test_user_can_add_item_into_cart(browser):
-    link='http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    link='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page=ProductPage(browser,link)
     page.open()
     page.add_item_into_cart()
