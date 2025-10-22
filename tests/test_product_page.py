d4dccd46c3ca906612934dd771bd401719e4d997
VladykaSith <naumov.vladyka@gmail.com>
2025-10-22 11:59:57 +0700

    moved methods to base_page.py and added 2 more tests into test_product_page.py

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,5 +1,6 @@
 from pages.product_page import ProductPage
 import pytest
+from pages.login_page import LoginPage
 links=["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -41,5 +42,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser,link):
     page.add_item_into_cart()
     page.should_disappear()
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link=links[0]
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page=LoginPage(browser,browser.current_url)
+    login_page.should_be_login_page()
 
