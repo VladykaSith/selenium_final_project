021f48ee6f9907616180efc35c2b1fb67cd17666
VladykaSith <naumov.vladyka@gmail.com>
2025-10-22 11:32:25 +0700

    added 3 tests test_guest_cant_see_success_message_after_adding_product_to_basket,test_guest_cant_see_success_message,test_message_disappeared_after_adding_product_to_basket

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,6 +1,6 @@
 from pages.product_page import ProductPage
 import pytest
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+links=["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
@@ -9,7 +9,8 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
+@pytest.mark.parametrize('link', links[0:2])
 def test_user_can_add_item_into_cart(browser,link):
 
     page=ProductPage(browser,link)
@@ -17,5 +18,28 @@ def test_user_can_add_item_into_cart(browser,link):
     page.add_item_into_cart()
     page.check_title()
     page.check_price()
+
+@pytest.mark.xfail(reason="Must fail by default")
+@pytest.mark.parametrize('link', links[0:2])
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser,link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_item_into_cart()
     page.should_not_be_success_message()
+
+@pytest.mark.parametrize('link', links[0:2])
+def test_guest_cant_see_success_message(browser,link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail(reason="Must fail by default")
+@pytest.mark.parametrize('link', links[0:2])
+def test_message_disappeared_after_adding_product_to_basket(browser,link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_item_into_cart()
+    page.should_disappear()
+
+
 
